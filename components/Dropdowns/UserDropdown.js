9f2324712f58e20770b30e37f6f58ad7060e1b54
nkoron <nkoron@itba.edu.ar>
2024-09-02 10:16:35 -0300

    agregue logout (hay q darle estilo)

--- components/Dropdowns/UserDropdown.js
+++ components/Dropdowns/UserDropdown.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createPopper } from "@popperjs/core";
+import {signOutAction} from "../../app/actions";
 
 const UserDropdown = () => {
   // dropdown props
@@ -36,51 +37,47 @@ const UserDropdown = () => {
           </span>
         </div>
       </a>
-      <div
-        ref={popoverDropdownRef}
-        className={
-          (dropdownPopoverShow ? "block " : "hidden ") +
-          "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
-        }
-      >
-        <a
-          href="#pablo"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          }
-          onClick={(e) => e.preventDefault()}
+        <div
+            ref={popoverDropdownRef}
+            className={
+                (dropdownPopoverShow ? "block " : "hidden ") +
+                "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+            }
         >
-          Action
-        </a>
-        <a
-          href="#pablo"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Another action
-        </a>
-        <a
-          href="#pablo"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Something else here
-        </a>
-        <div className="h-0 my-2 border border-solid border-blueGray-100" />
-        <a
-          href="#pablo"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Seprated link
-        </a>
-      </div>
+            <form action={signOutAction}>
+                <button type="submit" >
+                    Sign out
+                </button>
+            </form>
+            <a
+                href="#pablo"
+                className={
+                    "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                }
+                onClick={(e) => e.preventDefault()}
+            >
+                Another action
+            </a>
+            <a
+                href="#pablo"
+                className={
+                    "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                }
+                onClick={(e) => e.preventDefault()}
+            >
+                Something else here
+            </a>
+            <div className="h-0 my-2 border border-solid border-blueGray-100"/>
+            <a
+                href="#pablo"
+                className={
+                    "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                }
+                onClick={(e) => e.preventDefault()}
+            >
+                Seprated link
+            </a>
+        </div>
     </>
   );
 };
